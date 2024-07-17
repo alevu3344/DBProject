d4ad28f6823283258a9fcebcee1736cfbccba8ad
Alessandro Valmori <alevalmorii@gmail.com>
2024-07-17 14:41:35 +0200

    updated build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,29 +7,13 @@ repositories {
     mavenCentral()
 }
 
-val javaFXModules = listOf(
-    "base",
-    "controls",
-    "fxml",
-    "swing",
-    "graphics"
-)
-
-val supportedPlatforms = listOf("linux", "mac", "win") // All required for OOP
-
 
 dependencies {
     implementation("mysql:mysql-connector-java:8.0.29")
     testImplementation("org.assertj:assertj-core:3.25.3")
     testImplementation(libs.junit)
 
-    // JavaFX: comment out if you do not need them
-    val javaFxVersion = 15
-    for (platform in supportedPlatforms) {
-        for (module in javaFXModules) {
-            implementation("org.openjfx:javafx-$module:$javaFxVersion:$platform")
-        }
-    }
+
 }
 
 application {
